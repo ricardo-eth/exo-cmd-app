9bff0efb8ee6d892f076be57e8fc066ad6fd917e
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-03-18 14:45:30 +0100

    update

--- sayMyName.js
+++ sayMyName.js
@@ -1,2 +1,7 @@
-const name = process.argv[2]
+if (process.argv.length != 3) {
+  console.log(`usage: node sayHello.js name`)
+  process.exit(1)
+}
+
+let name = process.argv[2]
 console.log(`My name is, ${name}.`)
