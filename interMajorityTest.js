7d8c3c553da2a9325a5b4c6557cbac6dbb2abe08
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-03-19 11:49:34 +0100

    Update

--- interMajorityTest.js
+++ interMajorityTest.js
@@ -1,15 +1,15 @@
 const readlineSync = require('readline-sync')
 
+const userFistName = readlineSync.question('Quelle est votre prénom? ')
+const userLastName = readlineSync.question('Quelle est votre nom ? ')
+const ageStr = readlineSync.question('Quelle est votre age ? ')
+
 if (isNaN(process.argv[2])) {
   console.log(`Error: ${process.argv[2]} is not a number.`)
   process.exit(1)
 }
 
 let nb = Number(process.argv[2])
-
-const userFistName = readlineSync.question('Quelle est votre prénom? ')
-const userLastName = readlineSync.question('Quelle est votre nom ? ')
-const ageStr = readlineSync.question('Quelle est votre age ? ')
 
 const age = Number(ageStr)
 
