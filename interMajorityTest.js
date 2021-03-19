b4d42b00f31fa375480c52ffed393e083cdabb1e
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-03-19 12:21:54 +0100

    Update

--- interMajorityTest.js
+++ interMajorityTest.js
@@ -1,21 +1,30 @@
 const readlineSync = require('readline-sync')
 
+
+// Check commande line
+if (process.argv.length !== 3) {
+  console.log('usage : node majorityTest.js age')
+  process.exit(1)
+}
+
 if (isNaN(process.argv[2])) {
   console.log(`Error: ${process.argv[2]} is not a number.`)
   process.exit(1)
 }
 
-let nb = Number(process.argv[2])
+let MAJORITY = Number(process.argv[2])
 
-const userFistName = readlineSync.question('Quelle est votre prénom? ')
-const userLastName = readlineSync.question('Quelle est votre nom ? ')
-const ageStr = readlineSync.question('Quelle est votre age ? ')
 
-const age = Number(ageStr)
+while (true) {
+  const userFistName = readlineSync.question('Quelle est votre prénom? ')
+  const userLastName = readlineSync.question('Quelle est votre nom ? ')
+  const ageStr = readlineSync.question('Quelle est votre age ? ')
 
-if (age >= nb) {
-  console.log(`${userFistName} ${userLastName}, vous êtes majeur, vous pouvez voter`)
-} else {
-  console.log(`Désolé, ${userFistName} ${userLastName}, vous êtes mineur, vous ne pouvez pas voter`)
+  const age = Number(ageStr)
+
+  if (age >= MAJORITY) {
+    console.log(`${userFistName} ${userLastName}, vous êtes majeur, vous pouvez voter`)
+  } else {
+    console.log(`Désolé, ${userFistName} ${userLastName}, vous êtes mineur, vous ne pouvez pas voter`)
+  }
 }
-
--- majorityTest.js
+++ majorityTest.js
@@ -6,7 +6,9 @@ const ageStr = readlineSync.question('Quelle est votre age ? ')
 
 const age = Number(ageStr)
 
-if (age >= 18) {
+const MAJORITY = Number(18)
+
+if (age >= MAJORITY) {
   console.log(`${userFistName} ${userLastName}, vous êtes majeur, vous pouvez voter`)
 } else {
   console.log(`Désolé, ${userFistName} ${userLastName}, vous êtes mineur, vous ne pouvez pas voter`)
