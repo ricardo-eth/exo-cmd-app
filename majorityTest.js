8ce9532e53c995a1fe9bbbd7f7ef3537bfb814a1
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-03-24 11:27:22 +0100

    Update

--- majorityTest.js
+++ majorityTest.js
@@ -1,15 +1,46 @@
 const readlineSync = require('readline-sync')
 
-const userFistName = readlineSync.question('Quelle est votre prénom? ')
-const userLastName = readlineSync.question('Quelle est votre nom ? ')
-const ageStr = readlineSync.question('Quelle est votre age ? ')
+// Check command line
+if (process.argv.length !== 3) {
+  console.log('usage: node majorityTest.js age')
+  process.exit(1)
+}
 
-const age = Number(ageStr)
+if (isNaN(process.argv[2])) {
+  console.log(`Error: ${process.argv[2]} is not a number`)
+  process.exit(1)
+}
 
-const MAJORITY = Number(18)
+const majority = Number(process.argv[2])
 
-if (age >= MAJORITY) {
-  console.log(`${userFistName} ${userLastName}, vous êtes majeur, vous pouvez voter`)
-} else {
-  console.log(`Désolé, ${userFistName} ${userLastName}, vous êtes mineur, vous ne pouvez pas voter`)
+while (true) {
+  // get name and check if name is empty
+  const name = readlineSync.question('nom: ')
+  if (name.length === 0) {
+    console.log('Sorry, name is empty')
+    continue
+  }
+  // get first name and check if first name is empty
+  const firstName = readlineSync.question('firstName: ')
+  if (firstName.length === 0) {
+    console.log('Sorry, first name is empty')
+    continue
+  }
+
+  // get age and check if age is a number
+  const ageStr = readlineSync.question('age: ')
+  if (isNaN(ageStr) || ageStr.length === 0) {
+    console.log(`Sorry, ${ageStr} is not a number`)
+    continue
+  }
+
+  const age = Number(ageStr)
+
+  if (age >= majority) {
+    console.log(`${firstName} ${name} vous êtes majeur, vous pouvez voter.`)
+  } else {
+    console.log(
+      `Désolé, ${firstName} ${name}, vous êtes mineur, vous ne pouvez pas voter.`
+    )
+  }
 }
