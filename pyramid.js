75890f14eef0c9f125823d3f09bb079a5ac022af
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-03-24 12:34:46 +0100

    Update

--- pyramid.js
+++ pyramid.js
@@ -1,41 +1,48 @@
-const showStars = (nbStars, type, reverse) => {
-  
-  let str = ''
+// écrire un programme pyramid.js qui prend 2 arguments sur la ligne de commande
+
+/* Fonction */
+// let nbElem => nombre d'element de la base de ma pyramide
+// let charElem => le caractère qui représentera le motif
+// For NbElem { console.log(charElem.repeat(index)) }
+
+const pyramid = (nbElem, charElem, reverse) => {
   if (!reverse) {
-    for (let i = 1; i <= nbStars; ++i) {
-      str += type.repeat(i)
-      if (i !== nbStars) {
-        str += '\n'
-      }
-    }
-  } else {
-    for (let i = nbStars; i >= 1; --i) {
-      str += type.repeat(i)
-      if (i !== 1) {
-        str += '\n'
-      }
+    for (let i = 1; i <= nbElem; i += 1) {
+      console.log(charElem.repeat(i))
     }
   }
-  return str
+  else {
+    for (let i = nbElem; i >= 1; --i) {
+      console.log(charElem.repeat(i))
+    }
+  }
 }
 
-if (isNaN(process.argv[2])) {
-  console.log(`Error: ${process.argv[2]} is not a number.`)
+/* Boucles IF qui Test les erreurs */
+// 5 arguments 1=node 2=programme 3=argument (nbElem) 4=argument (charElem) 5=argument -r (Reverse)
+
+if (process.argv.length < 4 || process.argv.length > 5) {
+  console.log('La commande doit avoir 5 arguments: node pyramid.js nombreElement motif reverse')
   process.exit(1)
 }
 
-let nb = Number(process.argv[2])
-let type = process.argv[3]
-let reverseSwitch = process.argv[4]
-
-const controleReverse = (reverse) => {
-  if (reverse === '-r'){
-    return true
-  } else {
-    return false
-  }
+/* Boucles IF qui Test l'argument 5 */
+if (process.argv.length === 5 && process.argv[4] !== '-r') {
+  console.log("cet argument d'inversement de pyramid est incorrecte")
+  process.exit(1)
 }
 
-let reverse = controleReverse(reverseSwitch)
+/* Boucle basé sur les indexs */
+if (isNaN(process.argv[2])) {
+  console.log(`Error: ${process.argv[2]} nombreElement doit être un nombre`)
+  process.exit(1)
+}
 
-console.log(showStars(nb, type, reverse))
+/* argument entré à partir de l'index 3 */
+/* Index 0 et 1 et dédié à node et programme lancé */
+
+let nbElem = Number(process.argv[2])
+let charElem = process.argv[3]
+let isReverse = process.argv.length === 5 ? true : false
+
+pyramid(nbElem, charElem, isReverse)
